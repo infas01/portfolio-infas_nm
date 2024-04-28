13970c2df76ed1aa06462c771346922dff28aec6
Infas_NM <87600043+infas01@users.noreply.github.com>
2024-04-28 18:53:44 +0530

    error fixed

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">&copy; All rights reserved.</small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span>
+        This is a portfolio website hosted on Netlify.
+      </p>
+    </footer>
+  );
+}
